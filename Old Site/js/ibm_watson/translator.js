73f8333ff72cc2cec1244e7f33500e66d4ddcb9b
Jacob Tonna <34466548+Jtonna@users.noreply.github.com>
2020-01-30 18:35:47 -0800

    Updates
    Currently looping over all of the dom elements with an h1 tag, time to extract the inner text of each one and pass it to a function

--- Old Site/js/ibm_watson/translator.js
+++ Old Site/js/ibm_watson/translator.js
@@ -1,8 +1,39 @@
-var possileSelectors = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p']
+/*
+Plan
 
-console.log("---", possileSelectors)
+Loop over a list of possible selectors
+    for each selector find the inner text (inner html)
+        pass that inner html to the Translate Function
+            when it returns the data
+                replace the inner html (texr) on the dom with what watson returned to us.
+
+Create Translate Function
+    takes in 3 things ( current language, new language, content text )
+        pass that to the watson api
+        return the response (which should be the new text)
+
+
+*/
+
+const possileSelectors = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p']
+
+console.log("List of possible selectors", possileSelectors);
 
 var i;
 for (i = 0; i < possileSelectors.length; i++) {
-  console.log(possileSelectors[i])
+  console.log("Current Selector", possileSelectors[i])
 }
+
+//
+// Testing getting dom elements
+function getSelectorInnerText(selector){
+    console.log("Looking for ", selector)
+    return document.getElementsByTagName(String(selector))
+}
+
+console.log(getSelectorInnerText("h1"))
+
+// function myFunction() {
+//     const x = document.getElementById("myDIV").getElementsByTagName("P");
+//     document.getElementById("demo").innerHTML = x.length;
+//   }
