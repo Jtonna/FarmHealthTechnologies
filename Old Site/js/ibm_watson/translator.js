193037bf2a9bfae91add518d6916013fab2c89c0
Jacob Tonna <34466548+Jtonna@users.noreply.github.com>
2020-01-30 17:16:18 -0800

    Printing possible selectors

--- Old Site/js/ibm_watson/translator.js
+++ Old Site/js/ibm_watson/translator.js
@@ -1,7 +1,8 @@
-var r = "rick"
-var m = r + "morty"
+var possileSelectors = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p']
 
-console.log("***")
-console.log(r)
-console.log(m)
+console.log("---", possileSelectors)
 
+var i;
+for (i = 0; i < possileSelectors.length; i++) {
+  console.log(possileSelectors[i])
+}
