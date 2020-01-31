c82272775b9bd7344e1ac0dc56de7747a79002ea
Jacob Tonna <34466548+Jtonna@users.noreply.github.com>
2020-01-30 23:28:29 -0800

    Fixed HTML Collection Parsing Issue
    Inorder to loop over the HTML Collection, the HTML needs to be parsed  BEFORE the js. All i had to do was import the script after the body. Now i can iterate throught the HTMLCollection & set it to an array

--- Old Site/js/ibm_watson/translator.js
+++ Old Site/js/ibm_watson/translator.js
@@ -9,10 +9,28 @@
     Create a list of possible selectors
 
     loop over each possible selector
-        for each item in the HTMLCollection
-            add it's index & text content to an temp array
+        for the current selector
+            get the dom elements that have that selector
+                for each element
+                    add it's index & text content to an temp array
         for each item in the temp array
             pass it to the watson api function
             replace the current dom elements text with the response from the watson api
 
 */
+
+const selector = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p']
+const originalText = {}
+const translatedText = {}
+
+// Loop over every selector in the list
+for (let i = 0; i < selector.length; i++){
+
+    // Set the current selector to a variable as well as the HTMLCollection
+    currentSelector = selector[i]
+    currentCollection = document.getElementsByTagName(currentSelector);
+
+    console.log("current selector ", currentSelector)
+
+    
+}
