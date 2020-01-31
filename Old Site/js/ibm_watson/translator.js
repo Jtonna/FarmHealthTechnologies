3f5cb7f2903b1f4e41af6a56f1fbc5a023e83d0b
Jacob Tonna <34466548+Jtonna@users.noreply.github.com>
2020-01-30 22:05:45 -0800

    Finished blind testing, developing an actual plan

--- Old Site/js/ibm_watson/translator.js
+++ Old Site/js/ibm_watson/translator.js
@@ -1,39 +1,16 @@
-/*
-Plan
+/* PLAN
 
-Loop over a list of possible selectors
-    for each selector find the inner text (inner html)
-        pass that inner html to the Translate Function
-            when it returns the data
-                replace the inner html (texr) on the dom with what watson returned to us.
+    whats the problem?
+    I need to be able to translate from english into any desired language.
+    Inorder to do that we need to use curl commands with the watson api.
+    I need to be able to loop over every html innter text and send it one by one to watson's api for translation and replace the dom element with the new one.
 
-Create Translate Function
-    takes in 3 things ( current language, new language, content text )
-        pass that to the watson api
-        return the response (which should be the new text)
+    Possible solution ---
+    Create a list of possible selectors
+
+    for each possible selector
+        get all html elements
+            for each element, store its content in an temporary array
 
 
 */
-
-const possileSelectors = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p']
-
-console.log("List of possible selectors", possileSelectors);
-
-var i;
-for (i = 0; i < possileSelectors.length; i++) {
-  console.log("Current Selector", possileSelectors[i])
-}
-
-//
-// Testing getting dom elements
-function getSelectorInnerText(selector){
-    console.log("Looking for ", selector)
-    return document.getElementsByTagName(String(selector))
-}
-
-console.log(getSelectorInnerText("h1"))
-
-// function myFunction() {
-//     const x = document.getElementById("myDIV").getElementsByTagName("P");
-//     document.getElementById("demo").innerHTML = x.length;
-//   }
