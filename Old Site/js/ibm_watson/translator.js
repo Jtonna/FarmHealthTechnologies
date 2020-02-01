b89b4b044d8c983534311975c3f8e22e4e6e79c2
Jacob Tonna <34466548+Jtonna@users.noreply.github.com>
2020-02-01 01:35:03 -0800

    Trying to format the data to be added as a nested object. Calling it a night, will resume sometime tomorrow

--- Old Site/js/ibm_watson/translator.js
+++ Old Site/js/ibm_watson/translator.js
@@ -1,8 +1,8 @@
-const selector = ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6']
-const currentLanguage = "english"
+const selector = ['p', 'h1', ]//'h2', 'h3', 'h4', 'h5', 'h6']
+const originalLanguage = "english"
 const languageToTranslateTo = ""
 const textData = {}
-//const testingData = {"x":{0:{"english":"Hello World!", "spanish": "Hola mundo!"}}, "y":{0:{"english":"How are you doing today?", "spanish": "Cómo estás hoy?"}, 1:{"english":"one two three", "spanish":"uno dos tres"}}}
+const testingData = {"x":{0:{"english":"Hello World!", "spanish": "Hola mundo!"}}}
 
 function print(thingToPrint){
     console.log(thingToPrint)
@@ -13,6 +13,7 @@ for (var selector_loop = 0; selector_loop < selector.length; selector_loop++){
 
     // variable's
     const theHTMLCollection = document.getElementsByTagName(selector[selector_loop])
+    const selectorDataObj = {}
 
     print(`looking for ${selector[selector_loop]}`)
 
@@ -21,16 +22,35 @@ for (var selector_loop = 0; selector_loop < selector.length; selector_loop++){
 
     // If the html collection is > 0
     if (theHTMLCollection.length > 0){
-        print(`${theHTMLCollection}`)
+        print(theHTMLCollection)
 
         // loop theHtmlCollection.length number of times collection https://developer.mozilla.org/en-US/docs/Web/API/HTMLCollection
         const collectionItemCount = theHTMLCollection.length;
         for (var i = 0; collectionItemCount > i; i++){
+            const tempArrayObj = {}
+            const innerText = theHTMLCollection.item(i).innerText
+            // i represents the key, theHTMLCollection.item(i).innerText represents the value of the key (without the html)
             print(theHTMLCollection.item(i).innerText)
-        }
+            print(i)
 
+            // add the selector, key, language & information into the temp array
+            // print(`${selector[selector_loop]} ${i} ${originalLanguage} ${theHTMLCollection.item(i).innerText}`) // proof everything can be accessed
+            // tempArrayObj[selector[selector_loop]] = [i]
+            // tempArrayObj[selector[selector_loop]][i] =  {[originalLanguage]:[theHTMLCollection.item(i).innerText]}
+
+            tempArrayObj[selector[selector_loop]] 
+            //tempArrayObj[selector[selector_loop]]
+
+
+            print(`---\ntempArrayObj below`)
+            print(tempArrayObj)
+            print(testingData)
+
+        }
+    print("selector data below")
+    print(selectorDataObj)
 
     }
-
-    print(`\n`)
+    
+    print(`\n\n`)
 }
