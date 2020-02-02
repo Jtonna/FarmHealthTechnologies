67d8c9105a57ba70fdefa84f526d579a39de7e23
Jacob Tonna <34466548+Jtonna@users.noreply.github.com>
2020-02-01 23:57:32 -0800

    fixed data structure issue

--- Old Site/js/ibm_watson/translator.js
+++ Old Site/js/ibm_watson/translator.js
@@ -18,25 +18,18 @@ for (var selector_loop = 0; selector_loop < selector.length; selector_loop++){
 
     // checks to see if the length of items in the htmlcollection is greater than 0, prints true or false
     print(`HTMLCollection > 0 ::: ${theHTMLCollection.length > 0}`)
-
+    
     // If the html collection is > 0
     if (theHTMLCollection.length > 0){
         print(theHTMLCollection)
-
         // loop theHtmlCollection.length number of times collection https://developer.mozilla.org/en-US/docs/Web/API/HTMLCollection
         const collectionItemCount = theHTMLCollection.length;
         for (var i = 0; collectionItemCount > i; i++){
+            // temporary array for keeping data
             const tempArrayObj = {}
-            const innerText = theHTMLCollection.item(i).innerText
-            // i represents the key, theHTMLCollection.item(i).innerText represents the value of the key (without the html)
-            print(theHTMLCollection.item(i).innerText)
-            print(i)
-
             // add the selector, key, language & information into the temp array in the following structure
             // {"x":{"0":{"english":"Hello World!"}}}
-            tempArrayObj[selector[selector_loop]] = [i]
-            tempArrayObj[selector[selector_loop]][i] =  {[originalLanguage]:theHTMLCollection.item(i).innerText}
-
+            tempArrayObj[selector[selector_loop]] =  {[i]:{[originalLanguage]:theHTMLCollection.item(i).innerText}}
 
             print(`---\ntempArrayObj below`)
             print(tempArrayObj)
