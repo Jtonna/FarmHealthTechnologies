beb2e6c1446825d48cd770bdfdf74f1fe80cbfe0
Jacob Tonna <34466548+Jtonna@users.noreply.github.com>
2020-02-03 13:02:06 -0800

    Finished plan
    Fixed oversights on anchor tags, and reviewed the plan with a peer to cover all cases

--- Old Site/js/ibm_watson/translator.js
+++ Old Site/js/ibm_watson/translator.js
@@ -1,47 +1,66 @@
-const selector = ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6']
-const originalLanguage = "english"
-const languageToTranslateTo = ""
-const toBeTranslated = {}
+/*
 
-function print(thingToPrint){
-    console.log(thingToPrint)
-}
+* Plan
 
-function something(x){
+If the user doesnt have a translatorState object with a length bigger than 1 in their local storage
+    Loop over each dom element looking for a "selector", this should generate an HTMLCollection
+        for each item in the HTMLCollection
+            pass that value into the HTMLCollectionTempArray
+
+        for each item in the HTMLCollectionTempArray
+            add the data from HTMLCollectionTempArray in this style " selector{index{language:phrase}} " to the translatorState Object using the addTranslationToState function
     
-}
+    Once everything is done trigger the saveTranslatorState function
 
-// Loop over every selector
-for (var selector_loop = 0; selector_loop < selector.length; selector_loop++){
 
-    // variable's
-    const theHTMLCollection = document.getElementsByTagName(selector[selector_loop])
-
-    print(`looking for ${selector[selector_loop]}`)
-
-    // checks to see if the length of items in the htmlcollection is greater than 0, prints true or false
-    print(`HTMLCollection > 0 ::: ${theHTMLCollection.length > 0}`)
-    
-    // If the html collection is > 0
-    if (theHTMLCollection.length > 0){
-        print(theHTMLCollection)
-        // loop theHtmlCollection.length number of times collection https://developer.mozilla.org/en-US/docs/Web/API/HTMLCollection
-        const collectionItemCount = theHTMLCollection.length;
-        for (var i = 0; collectionItemCount > i; i++){
-            // add the selector, key, language & information into the temp array in the following structure
-            // {"x":{"0":{"english":"Hello World!"}}}
-            translatorState[selector[selector_loop]] =  {[i]:{[originalLanguage]:theHTMLCollection.item(i).innerText}}
-
-            // Add the current peice of data to a temp array holding data for all of the dom elements with the same selector
-            //Object.assign(selectorDataObj,tempArrayObj)
-
-            //print(`---\ntempArrayObj below`)
-            //print(tempArrayObj)
-
+create a addTranslationToState function that takes in 4 variables; selector, index, language & translated text
+    if the data we are going to add does NOT already exist in the translatorState Object
+        add the data to the translatorState Object in the form of the following data structure
+        translatorState = {
+            h:{
+                0:{english:"example text", spanish:"ejemplo de texto"}
+            }
         }
 
-    }
+
+create a saveTranslatorState function
+        pretty simple, just save the translatorState in the local storage
+
+
+create a shouldTranslateChecker function that takes in one variable "to_language"
+        if to_language is in the translatorState object's list of fully translated languages
+            trigger the translateFromLocalStorage function
+        else
+            trigger and pass the to_language to the startTranslationFunction
+
+
+create a startTranslation function that takes in a "to" language; our from will always be english
+    loop over the translatorState Object
+        for each "selector" in the object
+            for each index
+                grab the {language:phrase}, where the language is english
+                pass the phrase & the from & to language to the beginWatsonTranslation function, so it gets translated
+                when the translated phrase is returned pass it to the addTranslationToState Function to be stored
+                
+
+create a beginWatsonTranslation function that takes in 3 variables; the from & to language (say english -> spanish) & the test to translate
+    if max_attempts is < 10
+        pass the from_language, to_language & text to translate to the IBM Watson's Translation API
+        if the response is a time-out error or something along those lines
+            +1 max attempts
+            sleep for 1.5 second's or something like that
+            recursively call the beginWatsonTranslation & pass the data back into it with the current value of max_attempts
+        else
+            return the response
+    else
+        alert the user theres an error and we cant translate text right now & that they should come back later
+
+
+create a translateFromLocalStorage function that takes in one variable "to_language"
+        for each "selector"
+            and each selectors index in the translatorState object
+            replace the current DOM elements innerText with the "to_languages" version
     
-    print(`\n\n`)
-}
-print(toBeTranslated)
+*/
+
+const selector = ['p', 'a', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6']
