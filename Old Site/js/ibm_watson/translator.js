9f6523584b5cad97e5fd0b59029f15fa552a39f4
Jacob Tonna <34466548+Jtonna@users.noreply.github.com>
2020-02-02 21:11:53 -0800

    Update translator.js

--- Old Site/js/ibm_watson/translator.js
+++ Old Site/js/ibm_watson/translator.js
@@ -1,10 +1,14 @@
 const selector = ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6']
 const originalLanguage = "english"
 const languageToTranslateTo = ""
-const textData = {}
+const toBeTranslated = {}
 
 function print(thingToPrint){
     console.log(thingToPrint)
+}
+
+function something(x){
+    
 }
 
 // Loop over every selector
@@ -12,7 +16,6 @@ for (var selector_loop = 0; selector_loop < selector.length; selector_loop++){
 
     // variable's
     const theHTMLCollection = document.getElementsByTagName(selector[selector_loop])
-    const selectorDataObj = {}
 
     print(`looking for ${selector[selector_loop]}`)
 
@@ -25,23 +28,20 @@ for (var selector_loop = 0; selector_loop < selector.length; selector_loop++){
         // loop theHtmlCollection.length number of times collection https://developer.mozilla.org/en-US/docs/Web/API/HTMLCollection
         const collectionItemCount = theHTMLCollection.length;
         for (var i = 0; collectionItemCount > i; i++){
-            // temporary array for keeping data
-            const tempArrayObj = {}
             // add the selector, key, language & information into the temp array in the following structure
             // {"x":{"0":{"english":"Hello World!"}}}
-            tempArrayObj[selector[selector_loop]] =  {[i]:{[originalLanguage]:theHTMLCollection.item(i).innerText}}
+            translatorState[selector[selector_loop]] =  {[i]:{[originalLanguage]:theHTMLCollection.item(i).innerText}}
 
             // Add the current peice of data to a temp array holding data for all of the dom elements with the same selector
             //Object.assign(selectorDataObj,tempArrayObj)
 
-            print(`---\ntempArrayObj below`)
-            print(tempArrayObj)
+            //print(`---\ntempArrayObj below`)
+            //print(tempArrayObj)
 
         }
-    print("selector data below")
-    print(selectorDataObj)
 
     }
     
     print(`\n\n`)
 }
+print(toBeTranslated)
